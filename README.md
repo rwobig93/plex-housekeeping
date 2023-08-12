c96b83ecf0070c2e95dba346d2befd64e8c24f1c
rwobig93 <rwobig93@gmail.com>
2023-08-11 19:35:26 -0500

    Refactor to current standards, add movie title and file name enforcement

--- README.md
+++ README.md
@@ -11,23 +11,26 @@ git clone https://github.com/rwobig93/plex-housekeeping.git
 
 <h3> Current scripts list </h3>
 
-| Script Name     |          Script Options           |                        Purpose                         |
-|:----------------|:---------------------------------:|:------------------------------------------------------:|
-| plex-cleanup.py | [Options/Details](#plex-cleanup)  | Removes movie collections based on current movie count |
+| Script Name     |          Script Options           |                  Purpose                  |
+|:----------------|:---------------------------------:|:-----------------------------------------:|
+| plex-cleanup.py | [Options/Details](#plex-cleanup)  | Cleanup collections and movie title names |
 
 # Plex Cleanup
 
 ### Configuration
 
-<h4>NOTE: Running the script for the first time will generate a config file you can modify called plex-cleanup-config.json</h3>
+ > NOTE: Running the script for the first time will generate a config file you can modify called `plex-cleanup.json`
 
-| Setting Name                  |          Example           | Detail                                                                                                                                                                                                                       |
-|:------------------------------|:--------------------------:|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-| plex_url                      | https://192.168.1.1:32400/ | Required: URL pointing to your plex instance, can be public or private                                                                                                                                                       |
-| plex_api_key                  |    aBCde12F3gh4IJklmno5    | Required: API key from your Plex.TV account, please see [Finding Plex Token](https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/) for more details                                      |
-| movie_libraries               |  ["Movies","4K","Hidden"]  | Optional: List of your movie library names, any not included will be skipped                                                                                                                                                 |
-| minimum_collection_size       |             2              | Optional: Number indicating collection size for cleanup, collection cleanup will look at any collections with less movies than the number indicated here, so 2 would mean all collections with 1 or 0 movies will be cleaned |
-| delete_undersized_collections |            true            | Optional: Whether to delete collections based on the size indicated, if false then any collections that would be cleaned up will instead be printed to the terminal and logged, if true then collections will be deleted     |
+| Setting Name                         |          Example           | Detail                                                                                                                                                                                                                       |
+|:-------------------------------------|:--------------------------:|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+| plex_url                             | https://192.168.1.1:32400/ | Required: URL pointing to your plex instance, can be public or private                                                                                                                                                       |
+| plex_api_key                         |    aBCde12F3gh4IJklmno5    | Required: API key from your Plex.TV account, please see [Finding Plex Token](https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/) for more details                                      |
+| movie_libraries                      |  ["Movies","4K","Hidden"]  | Optional: List of your movie library names, any not included will be skipped                                                                                                                                                 |
+| minimum_collection_size              |             2              | Optional: Number indicating collection size for cleanup, collection cleanup will look at any collections with less movies than the number indicated here, so 2 would mean all collections with 1 or 0 movies will be cleaned |
+| delete_undersized_collections        |           false            | Optional: Whether to delete collections based on the size indicated, if false then any collections that would be cleaned up will instead be printed to the terminal and logged, if true then collections will be deleted     |
+| enforce_movie_names_match_file_names |           false            | Optional: Whether to enforce movie names to match file names after sanitization                                                                                                                                              |
+| movie_name_enforce_skip_characters   |    [':', '-', '.', '?']    | Optional: List of characters to ignore when comparing movie title and movie file names for name enforcement                                                                                                                  |
+| enforce_movie_names_exclude          |             []             | Optional: List of movie title names to ignore when enforcing movie title and file names, each entry is case sensitive and checks by doing a 'contains' operation          <br/>                                              |
 
 <h3>Installing required dependencies</h3>
 
--- plex-cleanup.py
+++ plex-cleanup.py
@@ -1,34 +1,52 @@
 # region Imports
+
+
 import json
 import logging
 import os.path
-from dataclasses import dataclass
+import re
+
+import urllib3
+import sys
+from enum import Enum
+from dataclasses import dataclass, field, asdict
 from os.path import exists
-from plexapi.server import PlexServer
+
+import requests
+from plexapi.server import PlexServer, Collection, Library
+from plexapi.video import Movie
+
 
 # endregion
 # region Classes
 
 
-@dataclass(init=False, repr=True, eq=True, order=True)
-class ScriptSettings:
+@dataclass
+class BaseClass:
+    def to_dict(self):
+        return asdict(self)
+
+
+class LogContentType(Enum):
+    STRING = 'string'
+    JSON = 'json'
+
+
+@dataclass(init=True, repr=True)
+class ScriptSettings(BaseClass):
     plex_url: str
     api_key: str
-    movie_libraries: list[str]
-    collection_size_minimum: int
-    delete_undersized_collections: bool
+    movie_libraries: list[str] = field(default_factory=lambda: ['Movies'])
+    collection_size_minimum: int = 2
+    delete_undersized_collections: bool = False
+    enforce_movie_names_match_file_names: bool = False
+    movie_name_enforce_skip_characters: list['str'] = field(default_factory=lambda: [':', '-', '.', '?'])
+    enforce_movie_names_exclude: list['str'] = field(default_factory=list)
 
-    def __init__(self, url, api, movie_library_names=None, delete_collections=False, size=-1) -> None:
-        if movie_library_names is None:
-            movie_library_names = list('Movies')
-        self.plex_url = url
-        self.api_key = api
-        self.movie_libraries = movie_library_names
-        self.collection_size_minimum = int(size)
-        self.delete_undersized_collections = delete_collections
-        if len(url) < 1:
+    def __post_init__(self) -> None:
+        if len(self.plex_url) < 1:
             raise ValueError('URL provided is empty, please provide a valid plex URL')
-        if len(api) < 1:
+        if len(self.api_key) < 1:
             raise ValueError('API provided is empty, please provide a valid plex API key')
 
 
@@ -36,179 +54,270 @@ class ScriptSettings:
 # region Globals
 
 
-global_settings: ScriptSettings
-plex_instance: PlexServer
-file_encoding = 'utf-8'
-path_config_file = './plex-cleanup-config.json'
+SETTINGS: ScriptSettings
+PLEX_INSTANCE: PlexServer
+FILE_ENCODING = 'utf-8'
 
 
 # endregion
 # region Functions
-
 # region Script Control
 
 
-def configure_logging(logfile_name='script_log'):
+def get_running_script_name() -> str:
+    script_path = os.path.abspath(__file__)
+    return str(os.path.basename(script_path)).replace('.py', '')
+
+
+def configure_logging() -> None:
     """ Configure the application logger """
+    path_log_file = f"{get_running_script_name()}.log"
     loglevel = logging.INFO
+
     if exists('./debug'):
         loglevel = logging.DEBUG
-    logging.basicConfig(filename='%s.log' % logfile_name, encoding=file_encoding, level=loglevel, format='%(asctime)s::%(levelname)s:%(message)s')
+
+    logging.basicConfig(filename=path_log_file, encoding=FILE_ENCODING, level=loglevel, format='%(asctime)s::%(levelname)s:%(message)s')
     logging.info('Logger initialized!')
 
 
-def stop_running_script(message, exception=None, error_code=1):
+def stop_running_script(message: str, exception: Exception = None, error_code: int = 1) -> None:
     """ Stop the running script with the provided message, exception and error code """
     if isinstance(exception, Exception):
-        full_message = 'Script was stopped due to a critical failure =>\n  %s =>\n    %s =>\n      %s' % (message, exception, exception.with_traceback)
+        full_message = ("Script was stopped due to a critical failure =>"
+                        f"\n  {message} =>\n    {exception} =>\n      {exception.with_traceback}")
     else:
-        full_message = 'Script was stopped due to a critical failure =>\n  %s' % message
+        full_message = f"Script was stopped due to a critical failure =>\n  {message}"
+
     logging.critical(full_message)
     print(full_message)
-    exit(error_code)
+
+    script_exit(error_code)
 
 
-def error_occurred(message, exception=None):
+def error_occurred(message: str, exception: Exception = None) -> None:
     """ Convey error occurrence with the provided error message and exception if one is provided """
     if isinstance(exception, Exception):
-        full_message = '%s =>\n  %s =>\n    %s' % (message, exception, exception.with_traceback)
+        full_message = f"{message} =>\n  {exception} =>\n    {exception.with_traceback}"
         logging.error(full_message)
         print(full_message)
     else:
         print(message)
 
 
-def print_and_log_message(message):
+def print_and_log_message(message: object, content_type: LogContentType = LogContentType.STRING) -> None:
     """ Print and log the provided message """
-    logging.info(message)
-    print(message)
+    content = str(message) if content_type == LogContentType.STRING else json.dumps(message, indent=4, default=str)
+
+    logging.info(content)
+    print(content)
 
 
-# endregion
-# region Configuration Handling
-
-
-def create_config_file():
+def create_config_file() -> None:
     """ Creates a default config file for modification """
+    path_config_file = f"{get_running_script_name()}.json"
+
     try:
-        default_config_file = {
-            "plex_url": "",
-            "plex_api_key": "",
-            "movie_libraries": ['Movies'],
-            "minimum_collection_size": 2,
-            "delete_undersized_collections": False
-        }
 
         if exists(path_config_file):
             os.remove(path_config_file)
-            logging.info('Deleted existing config file: %s' % path_config_file)
-        logging.debug('Attempting to create default config file at: %s' % os.path.abspath(path_config_file))
-        with open(path_config_file, 'w', encoding=file_encoding) as config_writer:
-            json.dump(default_config_file, config_writer)
-        logging.debug('Created default config file at: %s' % os.path.abspath(path_config_file))
+            logging.info(f"Deleted existing config file: {path_config_file}")
+
+        logging.debug(f"Attempting to create default config file at: {os.path.abspath(path_config_file)}")
+
+        with open(path_config_file, 'w', encoding=FILE_ENCODING) as config_writer:
+            json.dump(
+                ScriptSettings('https://plex-ip-or-hostname:32400/', '<insert_api_key_here>').to_dict(), config_writer)
+
+        logging.debug(f"Created default config file at: {os.path.abspath(path_config_file)}")
     except Exception as ex:
-        stop_running_script('Error occurred attempting to create config file at %s' % os.path.abspath(path_config_file), ex)
+        stop_running_script(
+            f"Error occurred attempting to create config file at {os.path.abspath(path_config_file)}", ex)
 
 
-def load_config_file():
+def load_config_file() -> None:
     """ Loads the script config file """
-    logging.debug('Attempting to read config file: %s' % path_config_file)
+    path_config_file = f"{get_running_script_name()}.json"
+
+    logging.debug(f"Attempting to read config file: {path_config_file}")
     try:
         if exists(path_config_file):
-            logging.info('Config file exists at %s' % path_config_file)
+            logging.info(f"Config file exists at {path_config_file}")
         else:
             create_config_file()
-            return_message = 'Config file wasn\'t found, created a new one at: %s' % os.path.abspath(path_config_file)
+            return_message = f"Config file wasn't found, created a new one at: {os.path.abspath(path_config_file)}"
             print(return_message)
             logging.info(return_message)
             exit(0)
-        with open(path_config_file, 'r', encoding=file_encoding) as config_reader:
+        with open(path_config_file, 'r', encoding=FILE_ENCODING) as config_reader:
             loaded_config = json.load(config_reader)
-            global global_settings
-            global_settings = ScriptSettings(loaded_config['plex_url'], loaded_config['plex_api_key'], loaded_config['movie_libraries'], loaded_config['minimum_collection_size'],
-                                             loaded_config['delete_undersized_collections'])
+            global SETTINGS
+            SETTINGS = ScriptSettings(**loaded_config)
     except Exception as ex:
-        stop_running_script('Failure occurred attempting to load the config file: %s' % path_config_file, ex)
+        stop_running_script(f"Failure occurred attempting to load the config file: {path_config_file}", ex)
+
+
+def script_startup() -> None:
+    configure_logging()
+    print_and_log_message("Starting script execution")
+
+    load_config_file()
+
+
+def script_exit(error_code: int = 0) -> None:
+    print_and_log_message('Finished script execution')
+
+    sys.exit(error_code)
 
 
 # endregion
-# region Plex API Handling
+# region Script Core
 
 
-def connect_to_plex_instance():
+def connect_to_plex_instance(plex_url: str, plex_key: str) -> None:
     """ Connect to the specified plex url and auth with the provided api key and return the instance object """
     try:
-        logging.info('Attempting to connect to plex instance at: %s' % global_settings.plex_url)
-        global plex_instance
-        plex_instance = PlexServer(global_settings.plex_url, global_settings.api_key)
-        logging.info('Successfully connected to plex instance at: %s' % global_settings.plex_url)
+        logging.info(f"Attempting to connect to plex instance at: {plex_url}")
+
+        session = requests.Session()
+        session.verify = False
+        urllib3.disable_warnings()
+
+        global PLEX_INSTANCE
+        PLEX_INSTANCE = PlexServer(plex_url, plex_key, session=session)
+
+        logging.info(f"Successfully connected to plex instance at: {plex_url}")
     except Exception as ex:
-        stop_running_script('Failure occurred attempting to connect to the provided plex url', ex)
+        stop_running_script("Failure occurred attempting to connect to the provided plex url", ex)
 
 
-def get_movie_collections():
+def get_movie_collections(movie_libraries: list[str], collection_size_minimum: int) -> list[Collection]:
     """ Return all movie collections from the connected plex instance, filters collections based on the minimum collection size provided in the config file """
     collection_count_total = 0
-    collection_filtered_list = []
-    collection_size_min = global_settings.collection_size_minimum
-    for movie_library in global_settings.movie_libraries:
+    collection_filtered_list: list[Collection] = []
+    collection_size_min = collection_size_minimum
+    for movie_library in movie_libraries:
         try:
-            logging.debug('Attempting to load collections from movie library: %s' % movie_library)
-            collections = plex_instance.library.section(movie_library).search(libtype='collection')
-            print_and_log_message('Library [%s] has a collection count of [%s]' % (movie_library, len(collections)))
-            logging.debug('Successfully grabbed movie library [%s], attempting to enumerate [%s] collections' % (movie_library, len(collections)))
+            logging.debug(f"Attempting to load collections from movie library: {movie_library}")
+
+            collections = PLEX_INSTANCE.library.section(movie_library).search(libtype='collection')
+            print_and_log_message(f"Library [{movie_library}] has a collection count of [{len(collections)}]")
+
+            logging.debug(f"Successfully grabbed movie library [{movie_library}], attempting to enumerate [{len(collections)}] collections")
+
             for collection in collections:
-                logging.debug('Enumerating collection, validating size: [collection_name]%s [collection_members]%s [member_minimum]%s'
-                              % (collection.title, collection.childCount, collection_size_min))
+                logging.debug(f"Enumerating collection, validating size: [collection_name]{collection.title}"
+                              f" [collection_members]{collection.childCount} [member_minimum]{collection_size_min}")
+
                 collection_count_total += 1
+
                 if collection_size_min > -1 and collection.childCount < collection_size_min:
-                    logging.debug('Found movie collection matching provided criteria, appending to master list: %s' % collection.title)
+                    logging.debug(f"Found movie collection matching provided criteria, appending to master list: {collection.title}")
                     collection_filtered_list.append(collection)
         except Exception as ex:
-            error_occurred('Failure occurred attempting to parse move library', ex)
-    print_and_log_message('Total filtered collections: %s' % len(collection_filtered_list))
-    print_and_log_message('Total collection count enumerated: %s from %s libraries' % (collection_count_total, len(global_settings.movie_libraries)))
+            error_occurred("Failure occurred attempting to parse move library", ex)
+
+    print_and_log_message(f"Total filtered collections: {len(collection_filtered_list)}")
+    print_and_log_message(f"Total collection count enumerated: {collection_count_total} from {len(movie_libraries)} libraries")
     return collection_filtered_list
 
 
-def delete_movie_collection(collection):
+def _delete_movie_collection(collection: Collection) -> None:
     """ Delete the provided movie collection from the connected plex instance """
     try:
         collection_name = collection.title
-        logging.debug('Attempting to delete moving collection: %s' % collection_name)
+        logging.debug(f"Attempting to delete moving collection: {collection_name}")
         collection.delete()
-        logging.info('Deleted movie collection: %s' % collection_name)
+        logging.info(f"Deleted movie collection: {collection_name}")
     except Exception as ex:
-        error_occurred('Failure occurred attempting to delete the provided collection: %s' % collection.title, ex)
+        error_occurred(f"Failure occurred attempting to delete the provided collection: {collection.title}", ex)
 
 
-def take_action_on_movie_collections(collections):
+def take_action_on_movie_collections(collections: list[Collection], delete_undersized_collections: bool = False) -> None:
     """ Deletes or conveys the provided movie collections list """
     for collection in collections:
-        if global_settings.delete_undersized_collections:
-            delete_movie_collection(collection)
+        if delete_undersized_collections:
+            _delete_movie_collection(collection)
         else:
-            print_and_log_message('We would delete this movie collection: %s' % collection.title)
+            print_and_log_message(f"We would delete this undersized movie collection: {collection.title}")
 
+
+def get_all_movies(movie_libraries: list[str]) -> list[Movie]:
+    all_movies = []
+
+    for library in movie_libraries:
+        try:
+            movie_library: Library = PLEX_INSTANCE.library.section(library)
+            library_movies: list[Movie] = movie_library.search(libtype='movie')
+
+            all_movies.extend(library_movies)
+            logging.debug(f"Gathered {len(library_movies)} movies from the {library} library")
+        except Exception as ex:
+            logging.error(f"Error occurred attempting to parse {library} movies: {ex}")
+
+    logging.info(f"Found a total of {len(all_movies)} movies from targeted libraries")
+    return all_movies
+
+
+def _sanitize_movie_name_for_file_match(movie_title: str, sanitize_pattern: str):
+    return re.sub(sanitize_pattern, "", movie_title).strip()
+
+
+def ensure_movie_name_matches_file(movies: list[Movie], make_changes: bool, characters_to_skip_for_match: list[str]) -> None:
+    logging.debug("Starting movie file and name match enforcement")
+    fixed_movie_count = 0
+    movie_title_sanitize_pattern = "|".join(map(re.escape, characters_to_skip_for_match))
+
+    for movie in movies:
+        # Verify provided exclude filters, if any are inside teh title of the movie we'll skip it
+        if any(exclude_name in movie.title for exclude_name in SETTINGS.enforce_movie_names_exclude):
+            logging.debug(f"Skipping matching movie in provided exclude list: {movie.title}")
+            continue
+
+        # Get the file name and discard the file extension of the movie
+        file_name, _ = os.path.splitext(os.path.basename(movie.media[0].parts[0].file))
+
+        # Extract and trim movie name | Movies have the year in the name following this format: movie name (movie_year).extension
+        file_movie_name = str(file_name).split('(')[0].strip()
+
+        logging.debug(f"Movie: {movie.title} | File: {file_movie_name}")
+        sanitized_title_name = _sanitize_movie_name_for_file_match(movie.title, movie_title_sanitize_pattern)
+        sanitized_file_name = _sanitize_movie_name_for_file_match(file_movie_name, movie_title_sanitize_pattern)
+
+        # Sanitized movie and file names match, so we'll move on
+        if sanitized_title_name == sanitized_file_name:
+            continue
+
+        logging.debug(f"Sanitized movie name doesn't match file name: {movie.title} != {file_movie_name}")
+
+        if make_changes:
+            movie.edit(**{"title.value": sanitized_file_name, "titleSort.value": sanitized_file_name})
+            fixed_movie_count += 1
+            logging.info(f"Updated Movie Title & Sort Title: {movie.title} => {sanitized_file_name}")
+
+    print_and_log_message(f"Finished movie name enforcement, fixed {fixed_movie_count} movies")
 
 # endregion
-
-
 # endregion
 
 
 def main():
     """ Main script execution point """
-    configure_logging('Plex-Cleanup-Script')
-    logging.info('Starting script execution')
-    load_config_file()
+    script_startup()
 
-    connect_to_plex_instance()
-    movie_collections = get_movie_collections()
-    take_action_on_movie_collections(movie_collections)
+    connect_to_plex_instance(SETTINGS.plex_url, SETTINGS.api_key)
 
-    logging.info('Finished script execution')
+    movie_collections = get_movie_collections(SETTINGS.movie_libraries, SETTINGS.collection_size_minimum)
+    take_action_on_movie_collections(movie_collections, SETTINGS.delete_undersized_collections)
+
+    all_movies = get_all_movies(SETTINGS.movie_libraries)
+    ensure_movie_name_matches_file(all_movies, SETTINGS.enforce_movie_names_match_file_names, SETTINGS.movie_name_enforce_skip_characters)
+
+    script_exit()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as root_exception:
+        stop_running_script("Global script failure occurred", root_exception, 1)
